9c3d629e1baf04b7c4ba59fb9fc27aecbd8235c4
kelseynyman <47357054+kelseynyman@users.noreply.github.com>
2022-05-26 16:07:02 -0400

    Add files via upload

---
+++ admin/view/header.php
@@ -0,0 +1,35 @@
+<?php
+// Create session
+session_start();
+
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['emploggedin'])) {
+    header('Location: login.php');
+    exit;
+}
+
+// Set commonly used variables
+$employee = $_SESSION['employee'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bank Name - <?php echo $title  ?></title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
